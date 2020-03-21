4b25a2c11aa0a92f756394e58718f7db5fd8b703
theslickestnick <nick.echev@gmail.com>
2020-03-21 15:29:26 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,15 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name 
+    artist.try(:name)
+    #if self.name evals true, run self.artist.name instead.
+    #if not, return ni
+  end
 end
